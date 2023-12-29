e8fd98a19176a22f852f0edfecb0e1a07c030855
oliverPavlanin <oliverpavlanin@gmail.com>
2023-12-29 15:55:33 +0100

    funkcne GUI

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-    std::cout << "Hello, World! oliver>" << std::endl;
+
+int main(){
+    int x = 100;
+    int y = 100;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Langston's Ant");
+    sf::Texture texture;
+    if (!texture.loadFromFile("images/ant.png")) {
+        std::cout << "Could not load enemy texture" << std::endl;
+        return 0;
+    }
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+
+        sf::Sprite image;
+        image.setTexture(texture);
+        window.clear();
+        window.draw(image);
+        window.display();
+
+    }
+
     return 0;
 }
